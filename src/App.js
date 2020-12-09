4bd19a55bf0a3771fdefba0b3a21f7e74b0b0c7f
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-09 14:13:38 -0500

    [Dino] Add state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Dino from "./Dino"
+import React, { Component } from 'react'
 
-function App(props) {
-  return (
-    <div className="App">
-      <Dino 
-        title={props.post.title}
-        author={props.post.author}
-        body={props.post.body}
-        comments={props.post.comments}
-      />
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Dino 
+          title={this.props.post.title}
+          author={this.props.post.author}
+          body={this.props.post.body}
+          comments={this.props.post.comments}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Dino.js
+++ src/Dino.js
@@ -1,14 +1,41 @@
-function Dino(props) {
-    console.log(props);
+import React, { Component } from 'react'
 
-    return (
-        <div>
-            <h1>Title: {props.title}</h1>
-            <p>Author: {props.author}</p>
-            <p>Body: {props.body}</p>
-            <h2>Comments</h2>
-        </div>
-    )
+class Dino extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          comment: this.props.comments[0],
+          editingComment: ""
+        };
+    }
+
+    changeEditingComment = (evt) => {
+        console.log(evt.target.value)
+        this.setState({
+            editingComment: evt.target.value
+        })
+    }
+
+    editComment = () => {
+        console.log(this.state.editingComment);
+        const newComment = this.state.editingComment;
+        this.setState({ comment: newComment })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Title: {this.props.title}</h1>
+                <p>Author: {this.props.author}</p>
+                <p>Body: {this.props.body}</p>
+                <h2>Comments</h2>
+                <p>{this.state.comment}</p>
+                <input value={this.state.editingComment} onChange={this.changeEditingComment}/>
+                <button onClick={this.editComment}>Edit</button>
+            </div>
+        )
+    }
 }
 
 export default Dino;
